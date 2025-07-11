9391807d8f08b13afcf05131d828ded689365850
DreamyK9 <63866130+DreamyK9@users.noreply.github.com>
2025-07-11 17:51:07 +0200

    add wezterm dotfile (was missing last commit)

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+local assets_folder = "C:/Users/jwolf/wezterm/assets"
+
+-- Shell
+config.default_prog = { "wsl.exe" }
+
+-- Font settings
+config.font_size = 12
+config.line_height = 1.2
+config.font = wezterm.font({ family = "SauceCodePro NFM", weight = "DemiBold" })
+
+-- Colors
+config.color_scheme = "Catppuccin Mocha"
+
+-- Appearance
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_image = assets_folder .. "/kitty_sky_blurred.png"
+
+return config
