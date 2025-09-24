11984115c23dc66229e01fb688a69931fbd51c7a
Wolf <jonas.wolf@hhi-extern.fraunhofer.de>
2025-09-24 13:51:23 +0200

    feat(wezterm): shortcut to open NixOS

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -19,5 +19,27 @@ config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_image = assets_folder .. "/kitty_sky_blurred_darkened.png"
 
-return config
+-- Events
+wezterm.on("spawn-nix-tab", function(window, pane)
+	-- local current_window = wezterm.mux.get_active_window()
+	window:mux_window():spawn_tab({
+		args = { "wsl.exe", "-d", "NixOS", "--cd", "~" },
+	})
+end)
 
+-- Keybindings
+config.leader = { key = "a", mods = "ALT", timeout_milliseconds = 1000 }
+
+config.keys = {
+	{
+		key = "r",
+		mods = "CTRL|ALT|SHIFT",
+		action = wezterm.action.ReloadConfiguration,
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.EmitEvent("spawn-nix-tab"),
+	},
+}
+return config
