b563a2285e55f5e79a8185e0b05c3f428eb08f1c
dharu <dharu@framewolf>
2025-10-20 19:19:10 +0200

    feat(wezterm): make background transparent

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -2,6 +2,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local assets_folder = ""
+local background_enabled = true
 
 -- Shell
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
@@ -29,7 +30,8 @@ config.color_scheme = "Catppuccin Mocha"
 -- Appearance
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_image = assets_folder .. "/kitty_sky_blurred_darkened.png"
+-- config.window_background_image = assets_folder .. "/kitty_sky_blurred_darkened.png"
+config.window_background_opacity = 0.88
 
 -- Events
 wezterm.on("spawn-nix-tab", function(window, pane)
