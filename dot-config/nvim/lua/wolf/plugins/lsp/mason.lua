640c241176a17436e9520cf0f6d22823c668811d
dharu <dharu@framewolf>
2025-11-01 14:29:56 +0100

    fix(nvim/lsp): change clang indent from 2 to 4

--- dot-config/nvim/lua/wolf/plugins/lsp/mason.lua
+++ dot-config/nvim/lua/wolf/plugins/lsp/mason.lua
@@ -20,24 +20,35 @@ return {
 			"pyright",
 			"lua_ls",
 			"bashls",
+			"clangd", -- c/c++ LSP implementation
 			-- Formatters
 			"prettier", -- JavaScript & Co. formatter
 			"eslint_d", -- JavaScript formatter
 			"stylua", -- lua formatter
 			"ruff", -- Blazingly fast, comprehensive python formatter and linter, written in Rust
-			"clangd", -- c/c++ LSP implementation
 		}
 
 		require("mason-lspconfig").setup({})
 
 		require("mason-tool-installer").setup({
-			automatic_installation = true,
+			automatic_installation = {
+				exclude = {
+					"clangd",
+				},
+			},
 			ensure_installed = tools,
 		})
 
+		vim.lsp.config["clangd"] = {
+			cmd = {
+				"clangd",
+				"--fallback-style=webkit",
+			},
+		}
+
 		-- Fixes vim.lsp complaining about missing config
-		for _, server in ipairs(tools) do
-			vim.lsp.config(server, {})
-		end
+		-- for _, server in ipairs(tools) do
+		-- 	vim.lsp.config(server, {})
+		-- end
 	end,
 }
